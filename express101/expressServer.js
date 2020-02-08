086d2fbe4196e8d9a951d945010bd626d33e10cf
sterlinghoughton@gmail.com <sterling.houghton@nike.com>
2020-02-07 19:32:53 -0800

    move node.jpeg to public folder, add middleware to expressServer.js

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 // all is a method that takes 2 args:
 // 1. route, 2. callback to run if the route is requested
-app.all('*', (req, res) => {
+app.all('/', (req, res) => {
   //Express handles hte basic headers, status code, mime-type etc.
   //Express handles the end (closing connection)
-  res.send('<h1>This is the home page.</h1>');
+  // res.send('<h1>This is the home page.</h1>');
+  res.sendFile(path.join(__dirname + '/node.html'));
 });
 
 app.listen(3000);
