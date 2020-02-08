d31f108ca9d09b58b6dc85c2affc46aee26fdc91
sterlinghoughton@gmail.com <sterling.houghton@nike.com>
2020-02-07 18:41:50 -0800

    create basic express configuration

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,3 +1,13 @@
-setInterval(() => {
-  console.log('yo yo');
-}, 1000);
+const express = require('express');
+
+const app = express();
+
+// all is a method that takes 2 args:
+// 1. route, 2. callback to run if the route is requested
+app.all('*', (req, res) => {
+  //Express handles hte basic headers, status code, mime-type etc.
+  //Express handles the end (closing connection)
+  res.send('<h1>This is the home page.</h1>');
+});
+
+app.listen(3000);
